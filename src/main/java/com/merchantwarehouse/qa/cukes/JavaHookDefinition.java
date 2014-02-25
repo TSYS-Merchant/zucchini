fb8bee30100f52a0e8043cb3d1114bb661f19471
cwarren-mw <cwarren@merchantwarehouse.com>
2014-02-25 12:01:25 -0500

    Adding cukes source

---
+++ src/main/java/com/merchantwarehouse/qa/cukes/JavaHookDefinition.java
@@ -0,0 +1,77 @@
+package com.merchantwarehouse.qa.cukes;
+
+import cucumber.api.Scenario;
+import cucumber.runtime.CucumberException;
+import cucumber.runtime.HookDefinition;
+import cucumber.runtime.MethodFormat;
+import cucumber.runtime.Utils;
+import cucumber.runtime.java.ObjectFactory;
+import gherkin.TagExpression;
+import gherkin.formatter.model.Tag;
+
+import java.lang.reflect.Method;
+import java.util.Collection;
+
+import static java.util.Arrays.asList;
+
+class JavaHookDefinition implements HookDefinition {
+
+    private final Class clazz;
+    private final Method method;
+    private final long timeoutMillis;
+    private final TagExpression tagExpression;
+    private final int order;
+    private final ObjectFactory objectFactory;
+
+    public JavaHookDefinition(final Class clazz, final Method method, final String[] tagExpressions, final int order,
+            final long timeoutMillis, final ObjectFactory objectFactory) {
+        this.clazz = clazz;
+        this.method = method;
+        this.timeoutMillis = timeoutMillis;
+        tagExpression = new TagExpression(asList(tagExpressions));
+        this.order = order;
+        this.objectFactory = objectFactory;
+    }
+
+    Method getMethod() {
+        return method;
+    }
+
+    @Override
+    public String getLocation(final boolean detail) {
+        MethodFormat format = detail ? MethodFormat.FULL : MethodFormat.SHORT;
+        return format.format(method);
+    }
+
+    @Override
+    public void execute(final Scenario scenario) throws Throwable {
+        Object[] args;
+        switch (method.getParameterTypes().length) {
+            case 0:
+                args = new Object[0];
+                break;
+            case 1:
+                if (!Scenario.class.equals(method.getParameterTypes()[0])) {
+                    throw new CucumberException("When a hook declares an argument it must be of type " +
+                            Scenario.class.getName() + ". " + method.toString());
+                }
+                args = new Object[]{scenario};
+                break;
+            default:
+                throw new CucumberException("Hooks must declare 0 or 1 arguments. " + method.toString());
+        }
+
+        Utils.invoke(objectFactory.getInstance(clazz), method, timeoutMillis, args);
+    }
+
+    @Override
+    public boolean matches(final Collection<Tag> tags) {
+        return tagExpression.evaluate(tags);
+    }
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+
+}
---
+++ src/main/java/com/merchantwarehouse/qa/cukes/MethodScanner.java
@@ -0,0 +1,86 @@
+package com.merchantwarehouse.qa.cukes;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.runtime.CucumberException;
+import cucumber.runtime.Utils;
+import cucumber.runtime.ClassFinder;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.List;
+
+import static cucumber.runtime.io.MultiLoader.packageName;
+import cucumber.runtime.java.StepDefAnnotation;
+
+class MethodScanner {
+    private final Collection<Class<? extends Annotation>> cucumberAnnotationClasses;
+
+    private final ClassFinder classFinder;
+
+    public MethodScanner(final ClassFinder classFinder) {
+        this.classFinder = classFinder;
+        cucumberAnnotationClasses = findCucumberAnnotationClasses();
+    }
+
+    /**
+     * Registers step definitions and hooks.
+     *
+     * @param javaBackend the backend where stepdefs and hooks will be registered
+     * @param gluePaths   where to look
+     */
+    public void scan(final JavaBackend javaBackend, final List<String> gluePaths) {
+        for (String gluePath : gluePaths) {
+            for (Class<?> glueCodeClass : classFinder.getDescendants(Object.class, packageName(gluePath))) {
+                while (glueCodeClass != null && glueCodeClass != Object.class && !Utils.isInstantiable(glueCodeClass)) {
+                    // those can't be instantiated without container class present.
+                    glueCodeClass = glueCodeClass.getSuperclass();
+                }
+                if (glueCodeClass != null) {
+                    for (Method method : glueCodeClass.getMethods()) {
+                        scan(javaBackend, method, glueCodeClass);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Registers step definitions and hooks.
+     *
+     * @param javaBackend   the backend where stepdefs and hooks will be registered.
+     * @param method        a candidate for being a stepdef or hook.
+     * @param glueCodeClass the class where the method is declared.
+     */
+    public void scan(final JavaBackend javaBackend, final Method method, final Class<?> glueCodeClass) {
+        for (Class<? extends Annotation> cucumberAnnotationClass : cucumberAnnotationClasses) {
+            Annotation annotation = method.getAnnotation(cucumberAnnotationClass);
+            if (annotation != null) {
+                if (!method.getDeclaringClass().isAssignableFrom(glueCodeClass)) {
+                    throw new CucumberException(String.format("%s isn't assignable from %s", 
+                            method.getDeclaringClass(), glueCodeClass));
+                }
+                if (isHookAnnotation(annotation)) {
+                    javaBackend.addHook(annotation, glueCodeClass, method);
+                } else if (isStepdefAnnotation(annotation)) {
+                    javaBackend.addStepDefinition(annotation, glueCodeClass, method);
+                }
+            }
+        }
+    }
+
+    private Collection<Class<? extends Annotation>> findCucumberAnnotationClasses() {
+        return classFinder.getDescendants(Annotation.class, "cucumber.api");
+    }
+
+    private boolean isHookAnnotation(final Annotation annotation) {
+        Class<? extends Annotation> annotationClass = annotation.annotationType();
+        return annotationClass.equals(Before.class) || annotationClass.equals(After.class);
+    }
+
+    private boolean isStepdefAnnotation(final Annotation annotation) {
+        Class<? extends Annotation> annotationClass = annotation.annotationType();
+        return annotationClass.getAnnotation(StepDefAnnotation.class) != null;
+    }
+}
